4f7ffba214adf9772217dc9314e59d3dc680b030
Andrew Bradley <cspotcode@gmail.com>
2021-07-19 23:08:53 -0400

    switch to node's builtin SourceMap impl

--- source-map-support.js
+++ source-map-support.js
@@ -1,4 +1,4 @@
-var SourceMapConsumer = require('source-map').SourceMapConsumer;
+const {SourceMap} = require('module');
 var path = require('path');
 
 var fs;
@@ -193,22 +193,24 @@ function mapSourcePosition(position) {
     // Call the (overrideable) retrieveSourceMap function to get the source map.
     var urlAndMap = retrieveSourceMap(position.source);
     if (urlAndMap) {
-      sourceMap = sourceMapCache[position.source] = {
-        url: urlAndMap.url,
-        map: new SourceMapConsumer(urlAndMap.map)
-      };
+      const mapJson = JSON.parse(urlAndMap.map);
 
       // Load all sources stored inline with the source map into the file cache
       // to pretend like they are already loaded. They may not exist on disk.
-      if (sourceMap.map.sourcesContent) {
-        sourceMap.map.sources.forEach(function(source, i) {
-          var contents = sourceMap.map.sourcesContent[i];
+      if (mapJson.sourcesContent) {
+        mapJson.sources.forEach(function(source, i) {
+          var contents = mapJson.sourcesContent[i];
           if (contents) {
-            var url = supportRelativeURL(sourceMap.url, source);
+            var url = supportRelativeURL(urlAndMap.url, source);
             fileContentsCache[url] = contents;
           }
         });
       }
+      sourceMap = sourceMapCache[position.source] = {
+        url: urlAndMap.url,
+        map: new SourceMap(mapJson)
+      };
+
     } else {
       sourceMap = sourceMapCache[position.source] = {
         url: null,
@@ -218,18 +220,25 @@ function mapSourcePosition(position) {
   }
 
   // Resolve the source URL relative to the URL of the source map
-  if (sourceMap && sourceMap.map && typeof sourceMap.map.originalPositionFor === 'function') {
-    var originalPosition = sourceMap.map.originalPositionFor(position);
+  if (sourceMap && sourceMap.map && typeof sourceMap.map.findEntry === 'function') {
+    var originalPosition = sourceMap.map.findEntry(position.line - 1, position.column);
 
     // Only return the original position if a matching line was found. If no
     // matching line is found then we return position instead, which will cause
     // the stack trace to print the path and line for the compiled file. It is
     // better to give a precise location in the compiled file than a vague
     // location in the original file.
-    if (originalPosition.source !== null) {
-      originalPosition.source = supportRelativeURL(
-        sourceMap.url, originalPosition.source);
-      return originalPosition;
+    if (originalPosition.originalSource != null) {
+      const source = supportRelativeURL(
+        sourceMap.url, originalPosition.originalSource);
+      const ret = {
+        source,
+        line: originalPosition.originalLine + 1,
+        column: originalPosition.originalColumn,
+        name: originalPosition.name
+      };
+      // console.dir({originalPosition, ret});
+      return ret;
     }
   }
 
